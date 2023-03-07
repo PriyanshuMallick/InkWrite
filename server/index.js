44882e720166c4d5be4cd498e14f68d482612d7a
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-03-07 11:58:39 +0530

    + Cors

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ console.log("Starting...");
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const authRouter = require("./routes/auth");
 
 const PORT = process.env.PORT;
@@ -10,6 +11,7 @@ const DB = process.env.DB;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(authRouter);
 
